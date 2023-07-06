4be9521ef9356edbd8f8a8124f25c64405ea3721
Titus K. Cheserem <cheseremtitus24@gmail.com>
2023-07-06 22:38:22 +0300

    modified:   9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,7 +3,8 @@
 This module contains the str function that
 returns the string representation of a float input
 """
-from typing import Iterable, Sequence
+from typing import Iterable, Sequence, Tuple, List
 
-def element_length(lst: List[]):
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     return [(i, len(i)) for i in lst]
